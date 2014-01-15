0f59a4da3fc0f47c78cb4654033f74d51358567d
App Academy Student <student@appacademy.io>
2014-01-15 15:04:41 -0500

    Fixes bug in Visit associations and adds Visit::record_visit

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,15 +1,21 @@
 class Visit < ActiveRecord::Base
   belongs_to(
     :visitor,
-    :class => "User",
+    :class_name => "User",
     :foreign_key => :visitor_id,
     :primary_key => :id
   )
 
   belongs_to(
     :url,
-    :class => "ShortenedUrl",
+    :class_name => "ShortenedUrl",
     :foreign_key => :url_id,
     :primary_key => :id
   )
+
+  def self.record_visit!(user, shortened_url)
+    v = Visit.new
+    v.visitor_id, v.url_id = user.id, shortened_url.id
+    v.save!
+  end
 end
