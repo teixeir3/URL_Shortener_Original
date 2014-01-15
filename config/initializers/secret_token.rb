ca03ac266a08e3397e1b42d4c569ce321b42a611
App Academy Student <student@appacademy.io>
2014-01-15 11:10:57 -0500

    Initial commit with CreateUsersTable migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+URLShortener::Application.config.secret_token = '95420e0d6e542d941f17ee5c5a6e4e99d30d0826ebd4516100d9d3d5b0cc6d38dbbdaff950e2b45fff5b73375c6acf92783e02d2800e1179a97c5ab3d287763d'
---
+++ db/migrate/20140115160137_create_users_table.rb
@@ -0,0 +1,12 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :fname
+      t.string :lname
+      t.string :email
+
+      t.timestamps
+    end
+  end
+
+end
