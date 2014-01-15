0ea0f899990effeaa495d19f2ca6ab97c37ea72d
App Academy Student <student@appacademy.io>
2014-01-15 11:41:56 -0500

    Adds associations between ShortenedUrl and User. Add validations for ShortenedUrl.

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,3 +1,12 @@
 class ShortenedUrl < ActiveRecord::Base
-  # attr_accessible :title, :body
+
+  validates :short_url, :uniqueness => true
+  validates :submitter_id, :short_url, :long_url, :presence => true
+
+  belongs_to(
+    :user,
+    :class_name => "User",
+    :foreign_key => :submitter_id,
+    :primary_key => :id
+  )
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   validates :email, :uniqueness => true
   validates :fname, :email, :lname, :presence => true
 
+  has_many(
+  :shortenedurls,
+  :class_name => "ShortenedUrl",
+  :foreign_key => :submitter_id,
+  :primary_key => :id
+  )
 end
